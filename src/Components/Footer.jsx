7b825f064b646ed3080cff177e780796da185c32
Sheikh Tanjim Hossain <tanjimhossain2003@gmail.com>
2025-06-03 05:09:26 +0600

    design change

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,12 +1,12 @@
 export default function Footer() {
   return (
-    <footer className="mt-auto pt-2 py-4 bg-gradient-to-b from-black/0 to-black/60 w-full">
+    <footer className="mt-auto pt-2 py-4 text-sm md:text-base bg-gradient-to-b from-black/0 to-black/60 w-full">
       <h1 className="text-lg text-center">
         Developed by{" "}
         <a
           href="https://www.linkedin.com/in/sheikh-tanjim-hossain-1753b4319/"
           target="_blank"
-          className=" hover:text-blue-700  underline"
+          className=" hover:text-blue-700 underline uppercase"
         >
           Sheikh Tanjim Hossain
         </a>
--- src/Components/Header/Logo.jsx
+++ src/Components/Header/Logo.jsx
@@ -1,9 +1,15 @@
-import logoImage from "../../assets/logo.svg";
+import logoImage from "../../assets/Wearher.png";
 
 export default function Logo() {
   return (
-    <a href="#">
-      <img className="h-9" src={logoImage} alt="Weather App" />
+    <a href="#" className="flex flex-row text-white">
+      <img className="h-12" src={logoImage} alt="Weather App" />
+      <div className="mx-2">
+        <h1 className="text-xl md:text-2xl uppercase">Weatheria</h1>
+        <p className="text-white text-xs md:text-sm -mt-1">
+          your everyday weather app
+        </p>
+      </div>
     </a>
   );
 }
